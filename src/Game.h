c02c93fbabde3b6fd844bbc5faec7ac51cfc6823
Maple_of_Ten <zhiyuzheng7@gmail.com>
2026-06-16 16:34:38 +0800

    Move shared game types out of Game

--- src/Game.h
+++ src/Game.h
@@ -16,69 +16,13 @@
 #include "Map.h"
 #include "Effects.h"
 #include "Config.h"
+#include "GameTypes.h"
 #include "RealtimeConfig.h"
 #include "AIController.h"
 #include "Building.h"
 #include "PathfindingService.h"
 #include "ResourceNode.h"
 #include "Worker.h"
-
-enum gameSeceneState {
-    SCENE_START,SCENE_GAME, SCEN_GAMEOVER
-};
-enum gamePlayer {
-    PLAYER,AI
-};
-
-namespace perk
-{
-    enum Type
-    {
-        Drill,
-        Fortitude,
-        Volley,
-        Charge,
-        SiegeCraft,
-        TowerCraft,
-        Logistics,
-        Mining,
-        WarChest,
-        Count
-    };
-}
-
-namespace gameop
-{
-    enum Type
-    {
-        SelectLane,
-        UpgradeEconomy,
-        UpgradeTech,
-        BuildBarracks,
-        BuildTower,
-        QueueUnit,
-        Count
-    };
-}
-
-struct GameOperation
-{
-    int type = gameop::SelectLane;
-    int laneIndex = lane::Mid;
-    int unitName = UName::INFANTARY;
-
-    GameOperation(int operationType = gameop::SelectLane, int lane = lane::Mid, int unit = UName::INFANTARY)
-        : type(operationType), laneIndex(lane), unitName(unit)
-    {
-    }
-};
-
-struct PerkChoice
-{
-    int type = perk::Drill;
-    std::string title;
-    std::string description;
-};
 
 class Game
 {
---
+++ src/GameTypes.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "AllUnit.h"
+#include "Config.h"
+
+#include <string>
+
+enum gameSeceneState
+{
+    SCENE_START,
+    SCENE_GAME,
+    SCEN_GAMEOVER
+};
+
+enum gamePlayer
+{
+    PLAYER,
+    AI
+};
+
+namespace perk
+{
+    enum Type
+    {
+        Drill,
+        Fortitude,
+        Volley,
+        Charge,
+        SiegeCraft,
+        TowerCraft,
+        Logistics,
+        Mining,
+        WarChest,
+        Count
+    };
+}
+
+namespace gameop
+{
+    enum Type
+    {
+        SelectLane,
+        UpgradeEconomy,
+        UpgradeTech,
+        BuildBarracks,
+        BuildTower,
+        QueueUnit,
+        Count
+    };
+}
+
+struct GameOperation
+{
+    int type = gameop::SelectLane;
+    int laneIndex = lane::Mid;
+    int unitName = UName::INFANTARY;
+
+    GameOperation(int operationType = gameop::SelectLane, int lane = lane::Mid, int unit = UName::INFANTARY)
+        : type(operationType), laneIndex(lane), unitName(unit)
+    {
+    }
+};
+
+struct PerkChoice
+{
+    int type = perk::Drill;
+    std::string title;
+    std::string description;
+};
